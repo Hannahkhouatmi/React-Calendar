08082e457004d3a1408a6cb71bdedfb2bc355e0f
Hannahkhouatmi <155753766+Hannahkhouatmi@users.noreply.github.com>
2024-12-04 18:54:29 +0100

    Initial commit

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import CreatingCalendar from './Calendar/Calendar.js';
+
+function App() {
+  return (
+    <div className="App">
+      <CreatingCalendar/>
+    </div>
+  );
+}
+
+export default App;
